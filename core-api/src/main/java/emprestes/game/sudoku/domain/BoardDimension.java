d0b76af51a68906c7567950c9b4612afe6b53aba
Prestes, E. M <emprestes@gmail.com>
2020-05-29 16:05:38 +0200

    #4 - Initial API defined.

--- core-api/src/main/java/emprestes/game/sudoku/domain/BoardDimension.java
+++ core-api/src/main/java/emprestes/game/sudoku/domain/BoardDimension.java
@@ -3,8 +3,8 @@ package emprestes.game.sudoku.domain;
 public enum BoardDimension {
     D9X9(9, 3), D16X16(16, 3);
 
-    public final int size;
-    private final int multiplicity;
+    public final Integer size;
+    private final Integer multiplicity;
 
     BoardDimension(int size, int multiplicity) {
         this.size = size;
@@ -12,10 +12,10 @@ public enum BoardDimension {
     }
 
     public Integer nextColumn(Integer column, Integer regionId) {
-        return column + (column == size ? 0 : (regionId % multiplicity) * multiplicity);
+        return column + (size.equals(column) ? 0 : (regionId % multiplicity) * multiplicity);
     }
 
     public Integer nextRow(Integer row, Integer regionId) {
-        return regionId % multiplicity == 0 ? (row + regionId) : row;
+        return (regionId % multiplicity) == 0 ? (row + regionId) : row;
     }
 }
